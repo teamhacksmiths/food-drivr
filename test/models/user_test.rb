a56ee4190cd73cdff094eb04b6b843102e9827e4
Ryan Collins <rccproductions@mac.com>
2016-04-02 17:54:01 -0400

    Test: add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,8 +20,9 @@ class UserTest < ActiveSupport::TestCase
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                           foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
-    @user.email = invalid_address
-    assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
   test "email addresses should be unique" do
     duplicate_user = @user.dup
