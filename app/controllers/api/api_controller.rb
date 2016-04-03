fedc703fe32c3caa4f159c81a9ee272a38c0f759
Ryan Collins <rccproductions@mac.com>
2016-04-03 14:32:48 -0400

    Feat: add basic authentication and middleware
    * The api_controller will authenticate a user using their auth_token, which should be
      * Passed in the header like: auth_token: some authentication token
    * The base api donation controller will then proceed to provide JSON methods for donations

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,21 @@
-class Api::ApiController < ApplicationController
+class BaseApiController < ApplicationController
+  before_filter :parse_request, :authenticate_user_with_token!
+
+  private
+    def authenticate_user_with_token!
+      if !@json['auth_token']
+        render nothing:  true, status: :unauthorized
+      else
+        @user = nil
+        User.find_each do |u|
+          if Devise.secure_compare(u.auth_token), @json['auth_token']
+            @user = u
+          end
+        end
+      end
+    end
+
+    def parse_request
+      @json = JSON.parse(request.body.read)
+    end
 end
