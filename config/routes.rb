fe228602d91643893f67d01f5d3258c2ce178711
Ryan Collins <rccproductions@mac.com>
2016-04-03 12:38:55 -0400

    Feat: scaffold out controllers for donations and base API

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ Rails.application.routes.draw do
   #   end
   # end
   # Api definition
-  # namespace :api, defaults: { format: :json },
-  #                             path: '/'  do
-  #                             scope module: :v1,
-  #                                       constraints: ApiConstraints.new(version: 1, default: true) do
-  #     # We are going to list our resources here
-  #     resources :users, :only => [:show]
-  #   end
-  # end
+  scope '/api' do
+    scope '/v1' do
+      scope '/donations' do
+        get '/' => 'api_donations#create'
+        post '/' => 'api_donations#create'
+      end
+    end
+  end
 end
