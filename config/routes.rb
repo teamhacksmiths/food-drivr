a607a52f512c0b02fc99e1efe02987a99a3e8fcf
Ryan Collins <rccproductions@mac.com>
2016-04-03 01:27:35 -0400

    Feat: add basic routing for rails API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -10,8 +11,10 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                             constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1 do
-
+                              scope module: :v1,
+                                        constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
 end
