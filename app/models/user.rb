4609292ea4f43633203d6453662bccfde2c5fdc0
Ryan Collins <rccproductions@mac.com>
2016-04-03 18:39:23 -0400

    Fix: user role was being called recursively killing the stack

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   before_create :generate_authentication_token!
   validates :auth_token, uniqueness: true
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,13 +10,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_many :donations, foreign_key: "donor_id", class_name: "Donation"
 
-  # Make sure to access with the role.description
+  # Access with the def role.
   belongs_to :role
-
-  def role
-    self.role.name
-  end
-
+  
   def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
