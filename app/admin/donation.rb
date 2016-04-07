ab65fe2648dddd0659fb527aa0bfb53b82aeae45
Ryan Collins <rccproductions@mac.com>
2016-04-07 13:22:35 -0400

    Fix: remove password setup in db seed

--- app/admin/donation.rb
+++ app/admin/donation.rb
@@ -1,34 +1,34 @@
-ActiveAdmin.register Donation do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+# ActiveAdmin.register Donation do
 #
-# permit_params :list, :of, :attributes, :on, :model
+# # See permitted parameters documentation:
+# # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+# #
+# # permit_params :list, :of, :attributes, :on, :model
+# #
+# # or
+# #
+# # permit_params do
+# #   permitted = [:permitted, :attributes]
+# #   permitted << :other if params[:action] == 'create' && current_user.admin?
+# #   permitted
+# # end
+#   config.batch_actions = false
 #
-# or
+#   scope :all, default: true
+#   DonationStatus.order(id: :asc).each do |s|
+#     scope(s.name) { |scope| scope.where("donations.status_id=?", s.id) }
+#   end
 #
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
+#   index do
+#     column("ID", :sortable => :id) {|d| link_to "#{d.id}", admin_donation_path(d) }
+#     column("Status")               {|d| status_tag(d.status.name) }
+#     column("Recipient")            {|d| link_to d.recipient.name, admin_recipient_path(d.recipient) }
+#     column("Donor")                {|d| link_to d.donor.name, admin_user_path(d.donor) }
+#     column("Driver")               {|d| link_to d.driver.name, admin_user_path(d.driver) }
+#     column "Initiated", :created_at
+#     actions
+#   end
+#
+#   permit_params :donor_id, :driver_id, :recipient_id, :status_id
+#
 # end
-  config.batch_actions = false
-
-  scope :all, default: true
-  DonationStatus.order(id: :asc).each do |s|
-    scope(s.name) { |scope| scope.where("donations.status_id=?", s.id) }
-  end
-
-  index do
-    column("ID", :sortable => :id) {|d| link_to "#{d.id}", admin_donation_path(d) }
-    column("Status")               {|d| status_tag(d.status.name) }
-    column("Recipient")            {|d| link_to d.recipient.name, admin_recipient_path(d.recipient) }
-    column("Donor")                {|d| link_to d.donor.name, admin_user_path(d.donor) }
-    column("Driver")               {|d| link_to d.driver.name, admin_user_path(d.driver) }
-    column "Initiated", :created_at
-    actions
-  end
-
-  permit_params :donor_id, :driver_id, :recipient_id, :status_id
-
-end
