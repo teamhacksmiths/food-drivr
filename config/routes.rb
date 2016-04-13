41e0e624f1d71a0e9cf7ab618364bfde4c897df0
Ryan Collins <rccproductions@mac.com>
2016-04-12 20:07:23 -0400

    Feat: add donor and driver namespaces and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,13 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     # TODO: enforce namespace / versioning: constraints: ApiConstraints.new(version: 1, default: true)
     namespace :v1 do
-      resources :users, :only => [:show, :create, :update, :destroy] do
+      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :donor do
         resources :donations, :only => [:create, :update, :destroy]
+      end
+      resources :driver, :only => [:index, :show] do
+        resources :dropoffs, :only => [:create, :update, :destroy]
+        resources :pickups, :only => [:create, :update, :destroy]
       end
       resources :donations, :only => [:show, :index]
       resources :sessions, :only => [:create, :destroy]
---
+++ spec/controllers/driver_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DriverController, type: :controller do
+
+end
