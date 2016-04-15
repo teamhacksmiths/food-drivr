a08213f3a6840857a01bb4c0777bcaf022952c5b
Ryan Collins <rccproductions@mac.com>
2016-04-15 13:52:45 -0400

    Request: please don't blow up

--- app/serializers/donation_serializer.rb
+++ app/serializers/donation_serializer.rb
@@ -2,8 +2,9 @@ class DonationSerializer < ActiveModel::Serializer
   attributes :id, :description, :created_at, :updated_at, :participants
   has_one :pickup
   has_one :dropoff
+  has_one :recipient
   has_many :donation_types
-  
+
   def participants
     { :donor => donor, :driver => driver}
   end
