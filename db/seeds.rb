653dcde44dec2c2f69929068226db57debdddc3e
Ryan Collins <rccproductions@mac.com>
2016-04-17 10:48:33 -0400

    Fix: reverse the changes to the seeds
    * See: https://github.com/teamhacksmiths/hackathon-for-hunger-backend/commit/d2d3ad2119fb70d05b7cc95b9f390025805cb217

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Define user roles
-# Role.create!(id: 0, description: 'Donor')
-# Role.create!(id: 1, description: 'Driver')
-# Role.create!(id: 2, description: 'Other')
-# Role.create!(id: 3, description: 'Unassigned')
-#
-# # Define donatan status
-# DonationStatus.create!(name: 'Pending', id: 0)
-# DonationStatus.create!(name: 'Active', id: 1)
-# DonationStatus.create!(name: 'Completed', id: 2)
-# DonationStatus.create!(name: 'Suspended', id: 3)
+Role.create!(id: 0, description: 'Donor')
+Role.create!(id: 1, description: 'Driver')
+Role.create!(id: 2, description: 'Other')
+Role.create!(id: 3, description: 'Unassigned')
+
+# Define donatan status
+DonationStatus.create!(name: 'Pending', id: 0)
+DonationStatus.create!(name: 'Active', id: 1)
+DonationStatus.create!(name: 'Completed', id: 2)
+DonationStatus.create!(name: 'Suspended', id: 3)
 
 # Run environment-specific seeds
 load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
