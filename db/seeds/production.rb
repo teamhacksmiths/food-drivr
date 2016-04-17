fa632b6b031cfdda8f182e17c6773b08c6f0f60c
Ryan Collins <rccproductions@mac.com>
2016-04-17 12:31:02 -0400

    Feat: add setup for seeding donation types

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -73,4 +73,24 @@ types = DonationType.all
                    recipient: recipients.sample,
                    description: FFaker::String,
                    status: statuses.sample)
+
+end
+
+# Create sample of types for donations
+donation = Donation.all
+
+# Loop through and create an array of type_ids
+donation_type_ids = []
+types.each do |t|
+  donation_type_ids << t.id
+end
+
+donation.each do |d|
+  # make sure that we are creating truly unique types
+  local_ids = donation_type_ids
+  type_id = local_ids.sample
+  Type.create!(donation_id: d.id, donation_type_id: type_id)
+  local_ids.delete type_id
+  next_type_id = local_ids.sample
+  Type.create!(donation_id: d.id, donation_type_id: next_type_id)
 end
