50245d1019579815f9faa1ba46e2dad22de6f31a
Ryan Collins <rccproductions@mac.com>
2016-04-17 12:43:03 -0400

    Fix: remove donation.rb database_exists method

--- app/admin/donation.rb
+++ app/admin/donation.rb
@@ -15,15 +15,13 @@ ActiveAdmin.register Donation do
   config.batch_actions = false
 
   scope :all, default: true
-  if database_exists?
-    DonationStatus.order(id: :asc).each do |s|
-      scope(s.name) { |scope| scope.where("donations.status_id=?", s.id) }
-    end
+  DonationStatus.order(id: :asc).each do |s|
+    scope(s.name) { |scope| scope.where("donations.status_id=?", s.id) }
   end
 
   index do
     column("ID", :sortable => :id) {|d| link_to "#{d.id}", admin_donation_path(d) }
-    column("Status")               {|d| status_tag(d.status.name) } if database_exists?
+    column("Status")               {|d| status_tag(d.status.name) }
     column("Recipient")            {|d| link_to d.recipient.name, admin_recipient_path(d.recipient) }
     column("Donor")                {|d| link_to d.donor.name, admin_user_path(d.donor) }
     column("Driver")               {|d| link_to d.driver.name, admin_user_path(d.driver) }
@@ -32,13 +30,6 @@ ActiveAdmin.register Donation do
     actions
   end
 
-  def database_exists?
-    ActiveRecord::Base.connection
-  rescue ActiveRecord::NoDatabaseError
-    false
-  else
-    true
-  end
 
   permit_params :donor_id, :driver_id, :recipient_id, :status_id
 
