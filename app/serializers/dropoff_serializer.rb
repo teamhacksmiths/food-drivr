6c6e1730658e67cc1c46bd22a9a062391709438e
Ryan Collins <rccproductions@mac.com>
2016-04-20 18:11:00 -0400

    Fix: remove status all together from pickup and dropoff

--- app/serializers/dropoff_serializer.rb
+++ app/serializers/dropoff_serializer.rb
@@ -1,3 +1,3 @@
 class DropoffSerializer < ActiveModel::Serializer
-  attributes :estimated, :actual, :latitude, :longitude, :street_address, :street_address_two, :city, :state, :zip, :status
+  attributes :estimated, :actual, :latitude, :longitude, :street_address, :street_address_two, :city, :state, :zip, :dropoffstatus
 end
--- app/serializers/pickup_serializer.rb
+++ app/serializers/pickup_serializer.rb
@@ -1,3 +1,3 @@
 class PickupSerializer < ActiveModel::Serializer
-  attributes :estimated, :actual, :latitude, :longitude, :street_address, :street_address_two, :city, :state, :zip, :status
+  attributes :estimated, :actual, :latitude, :longitude, :street_address, :street_address_two, :city, :state, :zip, :pickupstatus
 end
