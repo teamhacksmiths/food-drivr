9906164df5816002149d3404bf685b732f190c35
Ryan Collins <rccproductions@mac.com>
2016-04-22 18:53:58 -0400

    Feat rename pickup dropoff controller

--- app/controllers/api/v1/driver/donations/pickup_dropoff/dropoff_controller.rb
+++ app/controllers/api/v1/driver/donations/pickup_dropoff/dropoff_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Driver::Donations::DropoffController < ApplicationController
+class Api::V1::Driver::DropoffsController < ApplicationController
   before_action :authenticate_with_token!
   respond_to :json
 
