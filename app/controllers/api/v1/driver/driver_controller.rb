50882630d70deb5fe40d046a6d14b55ffa153c2c
Ryan Collins <rccproductions@mac.com>
2016-04-27 15:36:04 -0400

    Feat: add a check status method for drivers
    See: https://app.asana.com/0/94684923468934/110646733843612

--- app/controllers/api/v1/driver/driver_controller.rb
+++ app/controllers/api/v1/driver/driver_controller.rb
@@ -1,3 +1,11 @@
-class Api::V1::DriverController < ApplicationController
+class Api::V1::Driver::DriverController < ApplicationController
+  before_action :authenticate_with_token!
   skip_before_action :verify_authenticity_token
+
+  def check_status
+    @driver = current_user
+    render json: {
+      active: @driver.setting.active
+    }
+  end
 end
