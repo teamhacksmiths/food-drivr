a8c62da9a2f09563a95827366032713344d706e9
Ryan Collins <rccproductions@mac.com>
2016-05-02 15:37:34 -0400

    Style: add letter spacing to Devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:twitter,:facebook]
+         :omniauthable, :omniauth_providers => [:twitter, :facebook]
 
   before_create :generate_authentication_token!
   after_initialize :set_defaults
