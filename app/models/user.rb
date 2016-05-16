1bcf025600103ccdc826200aa2ed83f5d1fd5241
Ryan Collins <rccproductions@mac.com>
2016-05-16 15:08:08 -0400

    Chore: remove old method for updating password and move around class structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,23 +65,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  def update_with_password(params, *options)
-    current_password = params.delete(:current_password)
-    result =  if valid_password? current_password
-                update_attributes(params, *options)
-              else
-                self.assign_attributes(params, *options)
-                self.valid?
-                self.errors.add(:current_user, current_password.blank? ? :blank : :invalid)
-                false
-              end
-    clean_up_passwords
-    result
-  end
-
-  def clean_up_passwords
-    #TODO: clean up old passwords
-  end
 
   # Match the type of user to role and visa versa, providing backwords compatibility
   def match_type_to_role
@@ -95,5 +78,23 @@ class User < ActiveRecord::Base
         end
       end
     end
+  end
+
+
+  def update_with_password(params, *options)
+    current_password = params.delete(:current_password)
+    result =  if valid_password? current_password
+                update_attributes(params, *options)
+              else
+                self.assign_attributes(params, *options)
+                self.valid?
+                self.errors.add(:current_user, current_password.blank? ? :blank : :invalid)
+                false
+              end
+    clean_up_passwords
+    result
+  end
+  def clean_up_passwords
+    #TODO: Implement solution for cleaning up passwords
   end
 end
