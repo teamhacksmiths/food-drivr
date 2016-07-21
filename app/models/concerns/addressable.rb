076fc984ca23b5cff3cdb79cf7bbbb88c691b581
Ryan Collins <rccproductions@mac.com>
2016-07-20 21:52:02 -0400

    Fix: handle nulls in addressable module

--- app/models/concerns/addressable.rb
+++ app/models/concerns/addressable.rb
@@ -24,13 +24,21 @@ module Addressable
     full_address_changed? || street_address_changed? || street_address_two_changed? || city_changed? || zip_changed?
   end
 
+  def construct_address(address)
+    constructed_address = ""
+    constructed_address += address.number if address.number
+    constructed_address += address.street if address.street
+    constructed_address += address.street_type if address.street_type
+    constructed_address
+  end
+
   # Convenience for altering address based on hash values.
   # @params Options Hash with full street address components
   # EXAMPLE: { street_address: "123 Main St.", street_address_two: "Apt. 1",
   #            city: "Springfield", state: "MA", zip: "21111", country_code: "US" }
   def full_address=(address)
     new_address = StreetAddress::US.parse(address)
-    self.street_address = "#{new_address.number} #{new_address.street} #{new_address.street_type}"
+    self.street_address = construct_address(address)
     self.city = new_address.city
     self.state = new_address.state
     self.zip = new_address.postal_code
